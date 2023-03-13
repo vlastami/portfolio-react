2800266f986d763ae66822e2ac5a7f506f98911c
Vlasta Michalcova <vlasta.michalcova@email.cz>
2023-03-13 21:57:19 +0100

    init + insert old portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Skills from './components/Skills'
+import Certificates from './components/Certificates';
+import Footer from './components/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Projects from './components/Projects';
+import 'slick-carousel/slick/slick-theme.css';
+
+
 
 function App() {
+    useEffect(() => {
+      document.title = 'Vlasta Michalcová';
+      AOS.init();
+    }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-6 lg:px-20 xl:px-36 bg-beige">
+      <Navbar />
+      <Home />
+      <Skills />
+      <Projects />
+      <Certificates />
+      <Footer />
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "light": {
+                    "50": "#f9f9f9",
+                    "100": "#f0f0f0",
+                    "200": "#e6e6e6",
+                    "300": "#3c8067",
+                    "400": "#d2d2d2",
+                    "500": "#3c8067",
+                    "600": "#e8f7ef",
+                    "700": "#b4b4b4",
+                    "800": "#aaaaaa",
+                    "900": "#a0a0a0"
+                }
+            },
+        },
+    },
+    plugins: [],
+}
